81a394c249f0a23d2e18c1d76a14ee48b42f756c
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-07-20 21:18:23 -0500

    some map changes

--- maps/NewBarkTown.asm
+++ maps/NewBarkTown.asm
@@ -293,7 +293,7 @@ NewBarkTown_MapEvents:
 	coord_event  1,  9, SCENE_DEFAULT, NewBarkTown_TeacherStopsYouScene2
 
 	db 4 ; bg events
-	bg_event  8,  8, BGEVENT_READ, NewBarkTownSign
+	bg_event  3,  7, BGEVENT_READ, NewBarkTownSign
 	bg_event 11,  5, BGEVENT_READ, NewBarkTownPlayersHouseSign
 	bg_event  3,  3, BGEVENT_READ, NewBarkTownElmsLabSign
 	bg_event  9, 13, BGEVENT_READ, NewBarkTownElmsHouseSign
