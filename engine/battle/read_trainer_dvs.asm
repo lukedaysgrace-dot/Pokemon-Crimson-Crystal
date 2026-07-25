2dc75a9bb960db4d1e4fbf5b3ba15bdfe71605d4
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-07-25 18:27:39 -0500

    stat exp

--- engine/battle/read_trainer_dvs.asm
+++ engine/battle/read_trainer_dvs.asm
@@ -18,4 +18,34 @@ GetTrainerDVs:
 	pop hl
 	ret
 
+GetTrainerStatExp:
+; Return the stat exp of wOtherTrainerClass in bc, stored big endian
+; (b = high byte, c = low byte) so it can be written straight into a
+; party struct.
+
+	ld a, [wOtherTrainerClass]
+	and a
+	jr nz, .got_class
+; No trainer class to look up; give nothing rather than reading past the table.
+	ld bc, 0
+	ret
+
+.got_class
+	push hl
+	dec a
+	ld c, a
+	ld b, 0
+
+	ld hl, TrainerClassStatExp
+	add hl, bc
+	add hl, bc
+
+	ld a, [hli]
+	ld c, a
+	ld b, [hl]
+
+	pop hl
+	ret
+
 INCLUDE "data/trainers/dvs.asm"
+INCLUDE "data/trainers/stat_exp.asm"
