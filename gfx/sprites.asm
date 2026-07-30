a810a49cc2b961d3c63b85ab3983a4b8594c9ff4
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-07-30 17:14:30 -0500

    goldenrod power plant

--- gfx/sprites.asm
+++ gfx/sprites.asm
@@ -158,3 +158,4 @@ SECTION "Sprites 4", ROMX
 
 BattleGirlSpriteGFX::          INCBIN "gfx/sprites/battle_girl.2bpp"
 TamerSpriteGFX::               INCBIN "gfx/sprites/tamer.2bpp"
+EngineerSpriteGFX::            INCBIN "gfx/sprites/engineer_new.2bpp"
