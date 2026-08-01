7f1a9e9fbf5b7bdfb0a0bfb387a50f1dae5c8c8e
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-07-31 22:26:42 -0500

    new sprites

--- gfx/pokemon/grimer_alolan/anim.asm
+++ gfx/pokemon/grimer_alolan/anim.asm
@@ -1,12 +1,5 @@
-	frame 00, 08
-	frame 01, 18
-	frame 02, 10
-	frame 03, 05
-	frame 04, 05
-	frame 05, 05
-	frame 06, 05
-	frame 07, 05
-	frame 08, 05
-	frame 09, 05
-	frame 10, 05
+	frame 0, 04
+	frame 1, 12
+	frame 2, 08
+	frame 1, 28
 	endanim
