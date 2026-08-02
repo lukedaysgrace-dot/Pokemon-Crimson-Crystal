9850576e4d2a6bea6c7d3e822cbb24b66abdace1
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-02 14:47:37 -0500

    new sprites

--- gfx/pokemon/remoraid/anim.asm
+++ gfx/pokemon/remoraid/anim.asm
@@ -1,2 +1,14 @@
-	frame 0, 16
+; Frame order: 0, 1, 2, 3, 4, 5, 3, 4, 5, 6
+; Timing: 10 game frames per animation frame (~167 ms at 60 FPS)
+
+	frame 0, 10
+	frame 1, 10
+	frame 2, 10
+	frame 3, 10
+	frame 4, 10
+	frame 5, 10
+	frame 3, 10
+	frame 4, 10
+	frame 5, 10
+	frame 6, 10
 	endanim
