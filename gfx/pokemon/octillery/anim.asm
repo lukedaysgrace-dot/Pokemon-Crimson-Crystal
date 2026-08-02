dc93a8823788b76f666ae827ecc810f4a66f0e09
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-02 14:47:37 -0500

    new sprites

--- gfx/pokemon/octillery/anim.asm
+++ gfx/pokemon/octillery/anim.asm
@@ -1,2 +1,18 @@
-	frame 0, 16
+; Octillery animation
+; Sequence: 0,1,2,3,4,5,6,7,4,5,6,7,8,0
+
+	frame 0, 8
+	frame 1, 8
+	frame 2, 8
+	frame 3, 8
+	frame 4, 8
+	frame 5, 8
+	frame 6, 8
+	frame 7, 8
+	frame 4, 8
+	frame 5, 8
+	frame 6, 8
+	frame 7, 8
+	frame 8, 8
+	frame 0, 8
 	endanim
