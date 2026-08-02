edc61246677b385b214ef236531325783582ae91
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-02 14:47:37 -0500

    new sprites

--- gfx/pokemon/ampharos/anim.asm
+++ gfx/pokemon/ampharos/anim.asm
@@ -1,2 +1,13 @@
-	frame 0, 16
+; Frame order: 0, 2, 3, 2, 3, 2, 3, 4, 0
+; Timing: 8 game ticks per frame (closest Crystal timing to the 140 ms GIF)
+
+	frame 0, 8
+	frame 2, 8
+	frame 3, 8
+	frame 2, 8
+	frame 3, 8
+	frame 2, 8
+	frame 3, 8
+	frame 4, 8
+	frame 0, 8
 	endanim
