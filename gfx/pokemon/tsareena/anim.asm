3afc4d3dd1a09a95b2d0dc68240e15d850364703
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-02 23:03:04 -0500

    sprite updates

--- gfx/pokemon/tsareena/anim.asm
+++ gfx/pokemon/tsareena/anim.asm
@@ -1,2 +1,17 @@
-	frame 0, 30
+; Tsareena animation
+; 12 frames, extracted directly from the GIF
+; Original GIF timing: frames 0-6 = 150 ms, frames 7-11 = 100 ms
+
+	frame 0, 9
+	frame 1, 9
+	frame 2, 9
+	frame 3, 9
+	frame 4, 9
+	frame 5, 9
+	frame 6, 9
+	frame 7, 6
+	frame 8, 6
+	frame 9, 6
+	frame 10, 6
+	frame 11, 6
 	endanim
--- gfx/pokemon/ursaringbm/anim.asm
+++ gfx/pokemon/ursaringbm/anim.asm
@@ -1,2 +1,14 @@
-	frame 0, 16
+; Frame order: 0, 5, 1, 2, 3, 2, 3, 4, 5, 0
+; Timing: 13 game frames per animation frame (~217 ms at 60 FPS)
+
+	frame 0, 13
+	frame 5, 13
+	frame 1, 13
+	frame 2, 13
+	frame 3, 13
+	frame 2, 13
+	frame 3, 13
+	frame 4, 13
+	frame 5, 13
+	frame 0, 13
 	endanim
