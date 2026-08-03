1428a7a1686949735c467abadef19595665e55a5
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-03 15:57:28 -0500

    sprite updates

---
+++ gfx/pokemon/tsareena/frames.asm
@@ -0,0 +1,43 @@
+	dw .frame1
+	dw .frame2
+	dw .frame3
+	dw .frame4
+	dw .frame5
+	dw .frame6
+	dw .frame7
+	dw .frame8
+	dw .frame9
+	dw .frame10
+	dw .frame11
+.frame1
+	db $00 ; bitmask
+	db $31, $06, $06, $32, $33, $34, $35, $06, $36, $37, $38, $39
+	db $3a, $3b, $3c, $3d
+.frame2
+	db $01 ; bitmask
+	db $3e, $3c, $3d
+.frame3
+	db $00 ; bitmask
+	db $31, $06, $06, $32, $33, $34, $35, $06, $36, $37, $38, $39
+	db $3a, $3b, $3c, $3d
+.frame4
+	db $01 ; bitmask
+	db $3e, $3c, $3d
+.frame5
+	db $00 ; bitmask
+	db $31, $06, $06, $32, $33, $34, $35, $06, $36, $37, $38, $39
+	db $3a, $3b, $3c, $3d
+.frame6
+	db $02 ; bitmask
+.frame7
+	db $02 ; bitmask
+.frame8
+	db $03 ; bitmask
+	db $3f, $40, $41, $42, $43, $44, $45
+.frame9
+	db $02 ; bitmask
+.frame10
+	db $03 ; bitmask
+	db $3f, $40, $41, $42, $43, $44, $45
+.frame11
+	db $02 ; bitmask
