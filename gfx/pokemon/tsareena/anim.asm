8158df09f89f9fbd734829d48e2f26b96b42a091
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-08 22:54:30 -0500

    new sprites and some sprite replacements

--- gfx/pokemon/tsareena/anim.asm
+++ gfx/pokemon/tsareena/anim.asm
@@ -1,17 +1,19 @@
 ; Tsareena animation
-; 12 frames, extracted directly from the GIF
-; Original GIF timing: frames 0-6 = 150 ms, frames 7-11 = 100 ms
+; Source GIF timing preserved as closely as possible at 60 Hz.
+; Original frame durations (ms): 150, 150, 100, 170, 170, 170, 170, 100, 150, 150, 150, 150, 150
 
-	frame 0, 9
-	frame 1, 9
-	frame 2, 9
-	frame 3, 9
-	frame 4, 9
-	frame 5, 9
-	frame 6, 9
-	frame 7, 6
-	frame 8, 6
-	frame 9, 6
-	frame 10, 6
-	frame 11, 6
+TsareenaAnimation:
+	frame 0, 9	; 150 ms
+	frame 1, 9	; 150 ms
+	frame 2, 6	; 100 ms
+	frame 3, 10	; 170 ms
+	frame 4, 10	; 170 ms
+	frame 5, 10	; 170 ms
+	frame 5, 10	; 170 ms
+	frame 6, 6	; 100 ms
+	frame 0, 9	; 150 ms
+	frame 7, 9	; 150 ms
+	frame 0, 9	; 150 ms
+	frame 7, 9	; 150 ms
+	frame 0, 9	; 150 ms
 	endanim
