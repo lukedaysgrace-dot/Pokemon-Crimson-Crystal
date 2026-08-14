422295295e79bc4b5d0d08351ebe8d5cf0799c35
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-14 18:26:51 -0500

    new abilitys on mon update

--- data/pokemon/base_stats/gloom.asm
+++ data/pokemon/base_stats/gloom.asm
@@ -12,7 +12,7 @@
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/gloom/front.dimensions"
-	abilities_for GLOOM, CHLOROPHYLL, NO_ABILITY, STENCH
+	abilities_for GLOOM, CHLOROPHYLL, POISON_PUPPETEER, STENCH
 	db 0 ; padding
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_PLANT, EGG_PLANT ; egg groups
