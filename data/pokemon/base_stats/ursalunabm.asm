a2472f8754e91368b598fce68315fedb0a1e6e10
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-14 18:26:51 -0500

    new abilitys on mon update

--- data/pokemon/base_stats/ursalunabm.asm
+++ data/pokemon/base_stats/ursalunabm.asm
@@ -12,7 +12,7 @@
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/ursalunabm/front.dimensions"
-	abilities_for URSALUNABM, GUTS, BULLETPROOF, MINDS_EYE
+	abilities_for URSALUNABM, GUTS, BERSERK, MINDS_EYE
 	db 0 ; padding
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_GROUND ; egg groups
