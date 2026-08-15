7f48dbfb03632b70d91d3c8affa97d9109993052
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-15 11:56:39 -0500

    golett and golurk new sprites

--- gfx/pokemon/golett/anim.asm
+++ gfx/pokemon/golett/anim.asm
@@ -1,2 +1,20 @@
-	frame 0, 08
+; Golett animation
+; Source GIF: 12 frames at 110 ms each
+; Unique frames in sprite sheet: 4
+; GIF frame order: 0, 1, 0, 1, 0, 0, 2, 0, 3, 0, 2, 0
+; 7 game frames ~= 116.7 ms (closest Crystal timing to 110 ms)
+
+GolettAnimation:
+	frame 0, 7
+	frame 1, 7
+	frame 0, 7
+	frame 1, 7
+	frame 0, 7
+	frame 0, 7
+	frame 2, 7
+	frame 0, 7
+	frame 3, 7
+	frame 0, 7
+	frame 2, 7
+	frame 0, 7
 	endanim
