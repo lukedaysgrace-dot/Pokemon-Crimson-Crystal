41323dca2b14643ddfaaa97e4b043b631cc93397
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-15 18:03:11 -0500

    refresh of sprites

---
+++ gfx/pokemon/golurk/frames.asm
@@ -0,0 +1,23 @@
+	dw .frame1
+	dw .frame2
+	dw .frame3
+	dw .frame4
+	dw .frame5
+.frame1
+	db $00 ; bitmask
+	db $31, $32, $33, $34, $35, $36, $37, $38, $39, $3a, $3b, $3c
+	db $3d, $3e, $3f, $40, $41, $42
+.frame2
+	db $00 ; bitmask
+	db $31, $32, $33, $43, $44, $45, $37, $38, $39, $3a, $46, $47
+	db $3d, $3e, $3f, $40, $41, $48
+.frame3
+	db $01 ; bitmask
+	db $49, $4a
+.frame4
+	db $02 ; bitmask
+	db $4b, $4c, $4d, $4e, $4f, $50
+.frame5
+	db $03 ; bitmask
+	db $51, $52, $4b, $49, $4c, $4d, $4a, $4e, $4f, $50, $53, $54
+	db $55, $56, $57, $58
