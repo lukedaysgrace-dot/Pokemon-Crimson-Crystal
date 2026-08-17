01c64cb5abf37f94cf683c5f116b0a462e5d9414
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-17 16:59:19 -0500

    sprite updates

--- data/tilesets/johto_collision.asm
+++ data/tilesets/johto_collision.asm
@@ -105,11 +105,11 @@
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 68
 	tilecoll FLOOR, WALL, FLOOR, WALL ; 69
 	tilecoll WALL, UP_WALL, WALL, FLOOR ; 6a
-	tilecoll UP_WALL, WALL, FLOOR, WALL ; 6b
+	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 6b
 	tilecoll WALL, FLOOR, WALL, WALL ; 6c
 	tilecoll FLOOR, WALL, WALL, WALL ; 6d
 	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 6e
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 6f
+	tilecoll UP_WALL, WALL, FLOOR, WALL ; 6f
 	tilecoll UP_WALL, UP_WALL, FLOOR, FLOOR ; 70
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 71
 	tilecoll FLOOR, FLOOR, WALL, WALL ; 72
