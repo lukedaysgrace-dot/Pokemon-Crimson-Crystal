4cd1b63f6937adeffdac6e493e376aba6ab24d42
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-21 16:10:06 -0500

    skateboard dialogue updated

--- engine/phone/scripts/bike_shop.asm
+++ engine/phone/scripts/bike_shop.asm
@@ -1,5 +1,13 @@
 BikeShopPhoneCallerScript:
-	farwritetext UnknownText_0x174000
+	checkitem SKATEBOARD
+	iftrue .Skateboard
+	farwritetext BikeShopBicycleCallText
+	sjump .Finish
+
+.Skateboard:
+	farwritetext BikeShopSkateboardCallText
+
+.Finish:
 	clearflag ENGINE_BIKE_SHOP_CALL_ENABLED
 	specialphonecall SPECIALCALL_NONE
 	end
