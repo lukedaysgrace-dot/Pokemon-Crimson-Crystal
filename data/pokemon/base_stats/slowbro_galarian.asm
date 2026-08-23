a3bbe9e5ea653781fedbdb70f909fb5c6688c298
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-08-23 00:37:27 -0500

    silver party updates and entei dialogue

--- data/pokemon/base_stats/slowbro_galarian.asm
+++ data/pokemon/base_stats/slowbro_galarian.asm
@@ -12,7 +12,7 @@
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/slowbro_galarian/front.dimensions"
-	abilities_for SLOWBRO_GALARIAN, OWN_TEMPO, NO_ABILITY, REGENERATOR
+	abilities_for SLOWBRO_GALARIAN, OWN_TEMPO, UNAWARE, REGENERATOR
 	db 0 ; padding
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
--- data/pokemon/base_stats/slowking.asm
+++ data/pokemon/base_stats/slowking.asm
@@ -12,7 +12,7 @@
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/slowking/front.dimensions"
-	abilities_for SLOWKING, OBLIVIOUS, OWN_TEMPO, REGENERATOR
+	abilities_for SLOWKING, OBLIVIOUS, UNAWARE, REGENERATOR
 	db 0 ; padding
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
